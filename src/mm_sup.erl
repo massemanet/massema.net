3f609cdddb6c45df8eb307c1bf10a2251e3371f5
mats cronqvist <masse@cronqvi.st>
2018-02-12 16:28:36 +0100

    filthy start hack

--- src/mm_sup.erl
+++ src/mm_sup.erl
@@ -20,8 +20,9 @@ init(placeholder) ->
   {ok, {{one_for_one, 10, 60}, []}}.
 
 setup_cowboy() ->
-  Env = maps:from_list(application:get_all_env(mm)),
-  Dispatch = cowboy_router:compile([{'_', [{"/[...]", mm, Env}]}]),
+  Base = filename:dirname(filename:dirname(code:which(mm))),
+  Root = filename:join([Base,priv,static]),
+  Dispatch = cowboy_router:compile([{'_', [{"/[...]", mm, #{root=>Root}}]}]),
   {ok, _} = cowboy:start_http(mm_listener,
                               100,
                               [{port, 8080}],
