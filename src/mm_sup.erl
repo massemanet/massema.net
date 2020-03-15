6745321174240743c16febe94bc31d5c118d26b0
mats cronqvist <masse@cronqvi.st>
2020-03-15 11:44:35 +0100

    use rebar3 release

--- src/mm_sup.erl
+++ src/mm_sup.erl
@@ -20,8 +20,7 @@ init(placeholder) ->
   {ok, {{one_for_one, 10, 60}, []}}.
 
 setup_cowboy() ->
-  Base = filename:dirname(filename:dirname(code:which(mm))),
-  Root = filename:join([Base,priv,static]),
+  Root = filename:join([code:priv_dir('massema.net'),static]),
   Dispatch = cowboy_router:compile([{'_', [{"/[...]", mm, #{root=>Root}}]}]),
   {ok, _} = cowboy:start_http(mm_listener,
                               100,
